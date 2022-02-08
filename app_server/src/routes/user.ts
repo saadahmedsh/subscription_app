768e66b9d036266db584cd7a46a4385ee0b5a04f
saadahmedsh <67042577+saadahmedsh@users.noreply.github.com>
2022-02-08 14:21:26 +0500

    token authentication

--- app_server/src/routes/user.ts
+++ app_server/src/routes/user.ts
@@ -3,35 +3,43 @@ import User from "../schema/user";
 import { body, validationResult } from "express-validator";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from "express";
 
 const router = Router();
 
-const handleAuthentication=(req:Request, res:Response, next:NextFunction)=>{
-
-  let token=req.headers['authorization']
-  token=token?.split(' ')[1]
+const handleAuthentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token = req.headers["authorization"];
+  token = token?.split(" ")[1];
   try {
-    jwt.verify(token as string, 'subscriptionapp')
-    next()
-    
+    jwt.verify(token as string, "subscriptionapp");
+    next();
   } catch (error) {
-    
-      res.send(error)
-  }  
-  
-}
+    res.send(error);
+  }
+};
 
 router.post(
   "/register",
 
-  body("email").isEmail(),
-  body("password").isLength({ min: 5 }),
+  body("email").isEmail().withMessage("Invalid Email"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be atleast 5 characters"),
 
   (req, res) => {
     // validation checks
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      const errors = validationErrors.array().map((error) => {
+        return {
+          msg: error.msg,
+        };
+      });
+
       return res.send(errors);
     }
 
@@ -41,30 +49,30 @@ router.post(
 
     User.findOne({ email: email }, function (err: string, user: object) {
       if (err) {
-        res.json({
-          error: err,
-        });
+        res.json([{
+          msg: err
+        }]);
       }
 
       if (user) {
-        res.json({
-          exists: true,
-        });
+        res.json([{
+          msg: "Email already in use",
+        }]);
       } else {
         User.create({
           email: email,
           password: hashedPassword,
         });
 
-        res.json({
-          exists: false,
-        });
+        res.json([{
+          msg: 'Success',
+        }]);
       }
     });
   }
 );
 
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   User.findOne({ email: email }, function (err: string, user: User) {
@@ -77,36 +85,29 @@ router.get("/login", (req, res) => {
     if (user) {
       const match = bcrypt.compareSync(password, user.password);
       if (match) {
-        const token = jwt.sign(
-          { email: user.email },
-          'subscriptionapp',
-          {
-            expiresIn: 360000,
-          }
-        );
+        const token = jwt.sign({ email: user.email }, "subscriptionapp", {
+          expiresIn: 360000,
+        });
 
-        res.json({ user, token });
+        res.json([{msg:'success', user, token }]);
       } else {
-        res.json({ user: "Password did not match!" });
+        res.json([{ user: null, token:null, msg:'Password did not match' }]);
       }
     } else {
-      res.json({
-        user: "User does not exist",
-      });
+      res.json([{ user: null, token:null, msg:'User does not exist' }]);
     }
   });
 });
 
-router.get('/authorize', handleAuthentication, (req,res)=>{
-
-  let token=req.headers['authorization']
-  const {email, password} =req.body
-  token=token?.split(' ')[1]
+router.get("/authorize", handleAuthentication, (req, res) => {
+  let token = req.headers["authorization"];
+  const { email, password } = req.body;
+  token = token?.split(" ")[1];
   res.json({
-    token, email, password
-  })
-  
-
-})
+    token,
+    email,
+    password,
+  });
+});
 
 export default router;
--- src/components/Modal/Popup.tsx
+++ src/components/Modal/Popup.tsx
@@ -1,45 +1,65 @@
 import "./Popup.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal, Button, InputGroup, FormControl } from "react-bootstrap";
-import axios from 'axios'
-
-
-const handleClick =  () =>
-{
-  
-  const action=document.getElementById('action_button')?.getAttribute('value')
-  if (action == 'signup')
-  {
-    const email=document.getElementById('email')?.getAttribute('value')
-    const password=document.getElementById('password')?.getAttribute('value')
-
-    const data={
-      email,password
-    }
-
-    console.log(data)
-    
-    axios.post('http://localhost:8000/user/register', data)
-    .then(response => console.log(response)  )
-    .catch(error => {
-      console.log(error)
-    });
-
-  }
-
-}
+import axios from "axios";
+import { Alert } from "react-bootstrap";
 
 interface ModalProps {
   text: string;
   variant: "primary" | "secondary" | "danger";
-  action:"signup" | "login"
+  action: "signup" | "login";
 }
 
 export const Popup = ({ text, variant, action }: ModalProps) => {
   const [show, setShow] = useState(false);
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+  const [alert, showAlert] = useState(false);
+  const [alertText, changeText]=useState(' ')
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {setShow(true); showAlert(false)};
+
+
+  // Axios Api
+
+  const handleClick = () => {
+    const data = {
+      email,
+      password,
+    };
+    if (action == "signup") {
+    
+      axios
+        .post("http://localhost:8000/user/register", data)
+        .then((response) => {
+          changeText(response.data[0].msg)
+          showAlert(true)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    else
+    {
+      axios
+      .post("http://localhost:8000/user/login", data)
+      .then((response) => {
+
+        changeText(response.data[0].msg)
+        showAlert(true)
+        if (response.data[0].msg == 'success'){
+          localStorage.setItem('token', response.data[0].token)
+          
+        }
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+       
+    }
+  };
   return (
     <>
       <Button variant={variant} className="m-2" onClick={handleShow}>
@@ -51,29 +71,38 @@ export const Popup = ({ text, variant, action }: ModalProps) => {
           <Modal.Title>{text}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <InputGroup size="sm" className="mb-3" >
-            <InputGroup.Text  id="email">Email</InputGroup.Text>
+          <InputGroup size="sm" className="mb-3">
+            <InputGroup.Text id="email">Email</InputGroup.Text>
             <FormControl
               aria-label="Small"
               aria-describedby="email"
               type="email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </InputGroup>
 
-          <InputGroup size="sm" className="mb-3" >
+          <InputGroup size="sm" className="mb-3">
             <InputGroup.Text id="password">Password</InputGroup.Text>
             <FormControl
               aria-label="Small"
               aria-describedby="password"
-              type='password'
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </InputGroup>
         </Modal.Body>
         <Modal.Footer>
+          <Alert id="alert" show={alert} variant="danger">{alertText}</Alert>
+
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button value={action} id='action_button' variant="primary" onClick={handleClick}>
+          <Button
+            value={action}
+            id="action_button"
+            variant="primary"
+            onClick={handleClick}
+          >
             Save Changes
           </Button>
         </Modal.Footer>
